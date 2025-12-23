5ad679a9bbec455fe7c69701571151ca95dce114
Manuel  Luevano <manuelluevano@hotmail.com>
2025-12-23 16:09:17 -0600

    Deploy

--- src/components/BrandLogo.jsx
+++
@@ -1,7 +0,0 @@
-import logo from '../assets/FertiKhor_logo.svg'
-
-const BrandLogo = ({ className = '', ...props }) => (
-  <img src={logo} alt="Ferti Khor" className={`h-16 object-contain ${className}`} {...props} />
-)
-
-export default BrandLogo
--- src/controllers/aboutController.js
+++
@@ -1,4 +0,0 @@
-// Controlador: entrega el contenido para la página "Quiénes somos".
-import { aboutPageInfo } from '../models/aboutPage.js'
-
-export const getAboutContent = () => aboutPageInfo
